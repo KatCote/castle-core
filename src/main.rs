01504a6b8760ce9729056f1fdd7532a63864c3d7
KatCote <filtermr@gmail.com>
2023-10-08 07:23:35 -0500

    Build 0.0.6 03

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate castlecore;
 
 use castlecore::*;
+use crate::screen::*;
+use crate::render::*;
+use crate::border::*;
 
 fn main() { 
-    let _ = screen::initscr();
-    let _ = border::write_full_window(border::Screen::RenderLayer(render::RenderInterface::MapLayer(render::Map::Base)));
-    //let _ = border::write_full_window(border::Screen::Empty);
-    let _ = screen::usescr();
+    let _ = initscr();
+    let _ = write_full_window(Screen::RenderLayer(RenderInterface::MapLayer(Map::Base)));
+    //let _ = write_full_window(Screen::Empty);
+    let _ = usescr();
     println!("");
-    let _ = screen::endscr();
+    let _ = endscr();
 }

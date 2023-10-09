7edc357ed12c1a88b3a701f00865c910f5369a15
KatCote <filtermr@gmail.com>
2023-10-09 15:37:39 -0500

    Build 0.0.6 04

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use crate::border::*;
 
 fn main() { 
     let _ = initscr();
-    let _ = write_full_window(Screen::RenderLayer(RenderInterface::MapLayer(Map::Base)));
+    let _ = write_full_window(Screen::RenderLayer(RenderInterface::MapLayer(Map::SumObj(Summon::NPC))));
     //let _ = write_full_window(Screen::Empty);
     let _ = usescr();
     println!("");
     let _ = endscr();
 }
+
--- src/render.rs
+++ src/render.rs
@@ -2,7 +2,7 @@ use crate::functions::*;
 
 /// Enum for use in match in functions as param
 pub enum RenderInterface {
-    DEFAULT,
+    Default,
     MapFull,
     MapLayer(Map),
     InvPage(Inventory),
@@ -17,14 +17,24 @@ pub enum Map {
     Base,
     Color,
     Trigger,
-    Wall
+    Wall,
+    SumObj(Summon)
 }
 
 /// Inventory Pages enum for RenderInterface
 #[derive(Debug)]
 pub enum Inventory {
-    Page1,
-    Page2
+    Page1, // Like cells
+    Page2 // Like shells
+}
+
+/// Summon variations for RenderInterface (not logic or triggers)
+#[derive(Debug)]
+pub enum Summon {
+    Static, // Like chest (only render, not tringger or logic) // [][] of struct (x, y, type ... etc.)
+    Dynamic, //Like fireplace // [][] of struct (x, y, type ... etc.)
+    NPC, // Like trader in village // [][] of struct (x, y, type, hp, color ... etc.)
+    Enemy // Like golem // [][] of Enemy struct (x, y, hp, color ... etc.)
 }
 
 /// Render selected layer on current ractangle
@@ -33,25 +43,33 @@ pub fn render_layer(x1: u16, y1: u16, x2: u16, y2: u16, layer: RenderInterface) 
     for i in x1..x2 {
         for j in y1..y2 {
             match layer {
-                RenderInterface::DEFAULT => { let _ = printch(i, j, &'.'); },
-                RenderInterface::MapFull => { let _ = printch(i, j, &'0'); },
+                RenderInterface::Default => { let _ = printch(i, j, &'.'); },
+                RenderInterface::MapFull => { let _ = printch(i, j, &'1'); },
                 RenderInterface::MapLayer(ref ml) => {  
                     let _ = match ml {
                         Map::Base => printch(i, j, &'A'),
                         Map::Color => printch(i, j, &'B'),
                         Map::Trigger => printch(i, j, &'C'),
-                        Map::Wall => printch(i, j, &'D')
+                        Map::Wall => printch(i, j, &'D'),
+                        Map::SumObj(ref object) => {
+                            match object {
+                                Summon::Static => printch(i, j, &'!'),
+                                Summon::Dynamic => printch(i, j, &'@'),
+                                Summon::NPC => printch(i, j, &'#'),
+                                Summon::Enemy => printch(i, j, &'$')
+                            }   
+                        }
                     };
                 },
                 RenderInterface::InvPage(ref page) => {
-                    let _ =match page {
+                    let _ = match page {
                         Inventory::Page1 => printch(i, j, &'E'),
                         Inventory::Page2 => printch(i, j, &'F')
                     };
                 },
-                RenderInterface::PlayerParams => { let _ = printch(i, j, &'7'); },
-                RenderInterface::InfoWorld => { let _ = printch(i, j, &'8'); },
-                RenderInterface::InfoItem => { let _ = printch(i, j, &'9'); }
+                RenderInterface::PlayerParams => { let _ = printch(i, j, &'4'); },
+                RenderInterface::InfoWorld => { let _ = printch(i, j, &'5'); },
+                RenderInterface::InfoItem => { let _ = printch(i, j, &'6'); }
             };
         }
     }
